209aab7a4e2d839a75908594a44e5b7f69dcd610
Nimaga95 <117949986+Nimaga95@users.noreply.github.com>
2023-07-27 20:25:01 -0400

    initial commit

--- main.py
+++ main.py
@@ -1,35 +1,27 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
+screen.title("My Snake Game")
 screen.tracer(0)
 
-starting_position = [(0,0), (-20, 0), (-40, 0)]
-segments = []
-# Step 1 : Create the Snake Body : You can do it by many way
-for position in starting_position:
-    snake = Turtle(shape = "square")
-    snake.color("white")
-    snake.penup()
-    snake.goto(position)
-    segments.append(snake)
+snake = Snake()
+food = Food()
 
-# Step 2 : Make move the Snake
-screen.update()
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(1)
-    for element in segments:
-        element.forward(20)
-
-
-
-
-
-
+    time.sleep(0.1)
+    snake.move()
 
 
 screen.exitonclick()
